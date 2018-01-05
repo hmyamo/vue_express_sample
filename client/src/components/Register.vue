ed948121f3a041216c401d001c172c989a58b7e2
Hidenori Miyamoto <twentysecond.hide@gmail.com>
2018-01-05 18:14:54 +0900

    create songs model

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,42 +1,37 @@
 <template>
   <v-layout column>
     <v-flex xs6 offset-xs3>
-      <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
-        </v-toolbar>
-
-        <div class="pl-4 pr-4 pt-2 pb-2">
-          <form
-            name="vue-sample-form"
-            autocomplete="off">
-            <v-text-field
-              label="Email"
-              v-model="email"
-            ></v-text-field>
-            <br>
-            <v-text-field
-              label="Password"
-              type="password"
-              v-model="password"
-              autocomplete="new-password"
-            ></v-text-field>
-          </form>
-          <div class="error" v-html="error" />
+      <panel title="Register">
+        <form
+          name="vue-sample-form"
+          autocomplete="off">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
           <br>
-          <v-btn dark
-            class="cyan"
-            @click="register">
-            Register
-          </v-btn>
-        </div>
-      </div>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            autocomplete="new-password"
+          ></v-text-field>
+        </form>
+        <div class="error" v-html="error" />
+        <br>
+        <v-btn dark
+          class="cyan"
+          @click="register">
+          Register
+        </v-btn>
+      </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
+import Panel from '@/components/Panel'
 export default {
   data () {
     return {
@@ -58,6 +53,9 @@ export default {
         this.error = error.response.data.error
       }
     }
+  },
+  components: {
+    Panel
   }
 }
 </script>
